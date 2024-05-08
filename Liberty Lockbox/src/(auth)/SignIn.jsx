ba2e0dfaef244e387b47a51ff80be0915f2800fa
MilosGlamocak <milosglamocak@gmail.com>
2024-05-08 16:54:35 +0200

    functional login and signup + signout

--- Liberty Lockbox/src/(auth)/SignIn.jsx
+++ Liberty Lockbox/src/(auth)/SignIn.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { signIn } from '../../lib/appwrite'
 import { useState } from 'react'
+import { Link, Outlet } from 'react-router-dom'
 
 function SignIn() {
 
@@ -19,6 +20,8 @@ function SignIn() {
       <input placeholder='email' id='email' onChange={handleSetCredentials}/>
       <input placeholder='password' id='password' onChange={handleSetCredentials} type='password'/>
       <button onClick={handleLogIn}>Log In</button>
+      <Link to='signup'>Create new Account</Link>
+      <Outlet />
     </div>
   )
 }
--- Liberty Lockbox/src/(auth)/SignUp.jsx
+++ Liberty Lockbox/src/(auth)/SignUp.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { useState } from 'react';
+import { createUser } from '../../lib/appwrite';
+import { Link, Outlet, useNavigate } from 'react-router-dom';
 
 function SignUp() {
+  let navigate = useNavigate()
+
+  const [credentials, setCredentials] = useState({email: '', password: '', username:''})
+
+  const handleSetCredentials = (e) => {
+    const { id, value } = e.target;
+    setCredentials({...credentials, [id]: value});
+  }
+
+  const handleSignUp = () => {
+    createUser(credentials.email, credentials.password, credentials.username).then((res) => {
+      console.log(res);
+      navigate(-1)
+    })
+  }
   return (
-    <div>SignUp</div>
+    <div>
+      <input placeholder='email' id='email' onChange={handleSetCredentials}/>
+      <input placeholder='username' id='username' onChange={handleSetCredentials}/>
+      <input placeholder='password' id='password' onChange={handleSetCredentials} type='password'/>
+      <button onClick={handleSignUp}>Sign Up</button>
+      <Link to='/'>Already have an Account</Link>
+      <Outlet />
+    </div>
   )
 }
 

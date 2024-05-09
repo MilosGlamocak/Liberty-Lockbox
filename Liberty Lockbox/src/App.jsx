5b3d4893ff0856bf388706d9ee596489ba2c1a43
MilosGlamocak <milosglamocak@gmail.com>
2024-05-09 12:36:05 +0200

    useNavigate to root navigation + little styling

--- Liberty Lockbox/src/App.jsx
+++ Liberty Lockbox/src/App.jsx
@@ -6,42 +6,7 @@ import RouteContainer from './components/RouteContainer'
 import CustomRoutes from './components/CustomRoutes'
 
 function App() {
-
-  const {sessionId, username} = useAuth((state) => state)
-  const [credentials, setCredentials] = useState({email: '', password: '', username:''})
-
-  useEffect(() => {
-    checkForUser()
-  }, [])
-
-  const handleSetCredentials = (e) => {
-    const { id, value } = e.target;
-    setCredentials({...credentials, [id]: value});
-  }
-
-  const handleSignUp = () => {
-    createUser(credentials.email, credentials.password, credentials.username).then((res) => console.log(res))
-  }
-
-  /*const handleLogIn = () => {
-    signIn(credentials.email, credentials.password).then((res) => console.log(res))
-  }
-
-  const handleSignOut = () => {
-    signOut().then((res) => console.log(res))
-  }*/
-
-  return (/*
-    <>
-      <input placeholder='email' id='email' onChange={handleSetCredentials}/>
-      <input placeholder='username' id='username' onChange={handleSetCredentials}/>
-      <input placeholder='password' id='password' onChange={handleSetCredentials} type='password'/>
-      <button onClick={handleSignUp}>Sign Up</button>
-      <button onClick={handleLogIn}>Log In</button>
-      <button onClick={handleSignOut}>Sign Out</button>
-      <h2>{username}</h2>
-    </>*/
-
+  return (
     <CustomRoutes/>
   )
 }
--- Liberty Lockbox/src/components/RouteContainer.jsx
+++ Liberty Lockbox/src/components/RouteContainer.jsx
@@ -1,19 +1,30 @@
 import React from 'react'
 import { signOut } from '../../lib/appwrite'
-import { Link, Outlet } from 'react-router-dom'
+import { Link, Outlet, useNavigate } from 'react-router-dom'
+import {Container} from '@mui/material'
+import '../styles/RouteContainer.css'
+import { useAuth } from '../store'
 
 function RouteContainer() {
 
+  const username = useAuth((state) => state.username)
+  let navigate = useNavigate()
+
   const handleSignOut = () => {
     signOut().then((res) => console.log(res))
+    navigate('/')
   }
   return (
     <>
-      <button onClick={handleSignOut}>Sign Out</button>
-      <Link to='armory'>Armory</Link>
-      <Link to='profile'>Profile</Link>
+      <Container className='routeCont'>
+        <button onClick={handleSignOut}>Sign Out</button>
+        <Link to='armory'>Armory</Link>
+        <Link to='profile'>Profile</Link>
+        <h2>{username}</h2>
+      </Container>
       <Outlet />
     </>
+    
    
   )
 }

2004fa64e6654d370e45576d186904c84b5ec662
MilosGlamocak <milosglamocak@gmail.com>
2024-05-09 11:02:02 +0200

    added mui + routes in routecontainer

--- Liberty Lockbox/src/components/RouteContainer.jsx
+++ Liberty Lockbox/src/components/RouteContainer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { signOut } from '../../lib/appwrite'
+import { Link, Outlet } from 'react-router-dom'
 
 function RouteContainer() {
 
@@ -7,7 +8,13 @@ function RouteContainer() {
     signOut().then((res) => console.log(res))
   }
   return (
-    <button onClick={handleSignOut}>Sign Out</button>
+    <>
+      <button onClick={handleSignOut}>Sign Out</button>
+      <Link to='armory'>Armory</Link>
+      <Link to='profile'>Profile</Link>
+      <Outlet />
+    </>
+   
   )
 }
 

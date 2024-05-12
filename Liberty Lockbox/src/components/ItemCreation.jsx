af00aff90618bb211704d54fab8dc14cad0fc75b
MilosGlamocak <milosglamocak@gmail.com>
2024-05-12 13:12:37 +0200

    Migrated createNewItem to new folder

---
+++ Liberty Lockbox/src/components/ItemCreation.jsx
@@ -0,0 +1,56 @@
+import { Container } from '@mui/material'
+import React from 'react'
+import '../styles/ItemCreation.css'
+import { useState } from 'react'
+import { createNewItem } from '../../lib/appwrite'
+
+function ItemCreation() {
+
+    let [itemCred, setItemCred] = useState({
+        name: '',
+        image: '',
+        price: '',
+        chamber: '',
+        quantity: '',
+      })
+
+    const handleUpdateItemCred = (e) => {
+        const { id, value } = e.target;
+        setItemCred({...itemCred, [id]: value});
+        console.log(itemCred);
+        
+    }
+
+    const handleCreateNewItem = async () => {
+        createNewItem(
+          itemCred.name,
+          itemCred.image,
+          parseFloat(itemCred.price),
+          itemCred.chamber,
+          parseInt(itemCred.quantity)
+        ).then(() => {
+            window.location.reload();
+        })
+       
+      }
+
+      const inputFields = [
+        {type: 'text', id: 'name', placeholder: 'name'},
+        {type: 'text', id: 'image', placeholder: 'image'},
+        {type: 'number', id: 'price', placeholder: 'price'},
+        {type: 'text', id: 'chamber', placeholder: 'chamber'},
+        {type: 'number', id: 'quantity', placeholder: 'quantity'},
+
+      ]
+  return (
+    <Container className='itemCreationCont'>
+        <h2>Create new item</h2>
+        {inputFields.map((item) => {
+            return <input type={item.type} id={item.id} placeholder={item.placeholder} key={item.id} onChange={handleUpdateItemCred}/>
+        })}
+        <button onClick={handleCreateNewItem}>Publish item</button>
+    </Container>
+  )
+}
+
+export default ItemCreation
--- Liberty Lockbox/src/pages/Armory.jsx
+++ Liberty Lockbox/src/pages/Armory.jsx
@@ -3,40 +3,19 @@ import '../styles/Armory.css'
 import {Container} from '@mui/material'
 import {useAuth} from '../store'
 import { getAllItems, createNewItem, } from '../../lib/appwrite'
+import ItemCreation from '../components/ItemCreation'
 
 function Armory() {
 
   let [items, setItems] = useState([])
 
-  let [itemCred, setItemCred] = useState({
-    name: '',
-    image: '',
-    price: '',
-    chamber: '',
-    quantity: '',
-  })
+  
 
   useEffect(() => { 
     getAllItems().then((res) => {
       setItems(res.documents);
     })
   }, [])
-
-  const handleUpdateItemCred = (e) => {
-      const { id, value } = e.target;
-      setItemCred({...itemCred, [id]: value});
-      console.log(itemCred)
-  }
-
-  const handleCreateNewItem = async () => {
-    createNewItem(
-      itemCred.name,
-      itemCred.image,
-      parseFloat(itemCred.price),
-      itemCred.chamber,
-      parseInt(itemCred.quantity)
-    )
-  }
 
   const {label, username} = useAuth((state) => state)
   
@@ -45,19 +24,12 @@ function Armory() {
       {label === 'admin' && (
         <>
         <h2>Admin</h2>
-        <Container>
-          <input type="text" placeholder='name' id='name' onChange={handleUpdateItemCred}/>
-          <input type="text" placeholder='image' id='image' onChange={handleUpdateItemCred}/>
-          <input type="number" placeholder='price' id='price' onChange={handleUpdateItemCred}/>
-          <input type="text" placeholder='chamber' id='chamber' onChange={handleUpdateItemCred}/>
-          <input type="number" placeholder='quantity' id='quantity' onChange={handleUpdateItemCred}/>
-          <button onClick={handleCreateNewItem}>Publish item</button>
-        </Container>
+        <ItemCreation />
         </>
       
       )}
       <Container className='itemsCont'>
-        {items.map((item) => {
+        {items.reverse().map((item) => {
           return <Container key={item.name} style={{display: 'flex', flexDirection: 'row', maxWidth: '60%', justifyContent: 'center', alignItems: 'center'}}>
             <img src={item.image} style={{width: '300px'}}/>
             <Container>
